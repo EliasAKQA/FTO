7399dac50ae51b3decb70f14a6f3e2c069e36f84
Elias Geirdal <Elias.Geirdal@akqa.com>
2022-02-09 08:08:53 +0100

    no changes

--- BE/Flight2Orbit/Controller/HomeController.cs
+++ BE/Flight2Orbit/Controller/HomeController.cs
@@ -13,23 +13,23 @@ namespace Flight2Orbit.Controller
     public class HomeController : UmbracoApiController
     {
         //localhost:{port}/umbraco/api/home/gethomecontent
-        //public IHttpActionResult GetHomeContent()
-        //{
-        //    // Query Home node from DB
-        //    var homeNode = UmbracoContext.Content.GetByContentType(Home.GetModelContentType())?.FirstOrDefault();
-        //    // if it's null, return http response with status code 404 
-        //    if (homeNode == null) return NotFound();
-        //    // Convert to Home.  
-        //    var home = Converters.ConvertPublishedContent<Home>(homeNode);
+        public IHttpActionResult GetHomeContent()
+        {
+            // Query Home node from DB
+            var homeNode = UmbracoContext.Content.GetByContentType(Home.GetModelContentType())?.FirstOrDefault();
+            // if it's null, return http response with status code 404 
+            if (homeNode == null) return NotFound();
+            // Convert to Home.   
+            var home = Converters.ConvertPublishedContent<Home>(homeNode);
 
-        //    if (home.Sections == null) return NotFound();
-        //    List<SectionDTO> sections = new List<SectionDTO>();
-        //    foreach (var sectionPC in home.Sections)
-        //    {
-        //        var section = Converters.ConvertPublishedContent<Section>(sectionPC);
-        //        sections.Add(new SectionDTO(section.Header, section.Description, section.Image.Url(), new ButtonDTO(section.Text, section.Link)));
-        //    }
-        //    return Json(new HomeDTO(sections));
-        //}
+            if (home.Sections == null) return NotFound();
+            List<SectionDTO> sections = new List<SectionDTO>();
+            foreach (var sectionPC in home.Sections)
+            {
+                var section = Converters.ConvertPublishedContent<Section>(sectionPC);
+                sections.Add(new SectionDTO(section.Header, section.Description, section.Image.Url(), new ButtonDTO(section.Text, section.Link)));
+            }
+            return Json(new HomeDTO(sections));
+        }
     }
 }
