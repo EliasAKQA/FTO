f4fa71d805d751823494d9007d3ea8f4af54ed29
Elias Geirdal <Elias.Geirdal@akqa.com>
2022-02-14 14:52:35 +0100

    meta endpoint added

--- FE/src/pages/inventory/Inventory.js
+++ FE/src/pages/inventory/Inventory.js
@@ -26,15 +26,17 @@ const Inventory = () => {
             <section className={"inventory__header"}>
                 <h1>Inventory</h1>
                 <h2>TIME FOR ACTION</h2>
-                <p>Resources like food and water are constantly being used by the crew on the ISS. That is why the staion needs to be restocked. Here you can see how much food and water is currently lrft on the ISS and for how long the crew can last with that amount.</p>
+                <p>Resources like food and water are constantly being used by the crew on the ISS. That is why the
+                    staion needs to be restocked. Here you can see how much food and water is currently lrft on the ISS
+                    and for how long the crew can last with that amount.</p>
 
 
             </section>
             <section className={"inventory__timer"}>
-                    <h2>TIME REMAINING</h2>
-                    {time &&
-                        <Clock initHour={time.hour} initMinute={time.minute} initSecond={time.second}/>
-                    }
+                <h2>TIME REMAINING</h2>
+                {time &&
+                    <Clock initHour={time.hour} initMinute={time.minute} initSecond={time.second}/>
+                }
             </section>
             <section className={"resource__container"}>
                 <Resource height={"43%"} type={"food"}/>
@@ -42,10 +44,12 @@ const Inventory = () => {
             </section>
             <section className={'section-container'}>
                 <h2>What to help the crew out?</h2>
-            <p>We are selling meteoriters found and signed by our astronauts at our shop! The money we make by selling these meteorites is then used to resupply the ISS with the neccessary resources they need.</p>
-            <Link to={"/shop"} className={"btn btn--primary "}>
-                SHOP NOW
-            </Link>
+                <p>We are selling meteoriters found and signed by our astronauts at our shop! The money we make by
+                    selling these meteorites is then used to resupply the ISS with the neccessary resources they
+                    need.</p>
+                <Link to={"/shop"} className={"btn btn--primary "}>
+                    SHOP NOW
+                </Link>
             </section>
         </div>
     );
