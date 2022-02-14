58682416dc9c33608e24c403852ccd65995a4ed3
Elias Geirdal <Elias.Geirdal@akqa.com>
2022-02-14 10:08:09 +0100

    quiz api endpoint added.

---
+++ BE/Flight2Orbit/Models/Quiz/QuizDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Flight2Orbit.Models.Quiz
+{
+    public class QuizDTO
+    {
+        public string Headline { get; set; }
+        public string Text { get; set; }
+        public IEnumerable<QuizThemesDTO> Themes { get; set; }
+        public QuizDTO(string headline, string text, IEnumerable<QuizThemesDTO> themes)
+        {
+            Headline = headline;
+            Text = text;
+            Themes = themes;
+        }
+    }
+}
---
+++ BE/Flight2Orbit/Models/Shop/ShopItemDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace Flight2Orbit.Models
+{
+    public class ShopItemDTO
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public ButtonDTO Button { get; set; }
+
+        public ShopItemDTO(int id, string imageUrl, string title, double price, ButtonDTO button)
+        {
+            Id = id;
+            ImageUrl = imageUrl;
+            Title = title;
+            Price = price;
+            Button = button;
+        }
+    }
+}
