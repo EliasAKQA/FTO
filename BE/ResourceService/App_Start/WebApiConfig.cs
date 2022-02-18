d14a3eaaeba36999b088b1803c9b3da980045277
Elias Geirdal <Elias.Geirdal@akqa.com>
2022-02-18 11:19:26 +0100

    resourceservice added and working

--- BE/ResourceService/App_Start/WebApiConfig.cs
+++ BE/ResourceService/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace ResourceService
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{method}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
