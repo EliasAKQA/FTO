06eeb919b7ca65bad182b6dbc2876f7d6ca839cd
Elias Geirdal <Elias.Geirdal@akqa.com>
2022-02-21 13:09:56 +0100

    session service added

---
+++ BE/ProxyService/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using ProxyService.Services;
+
+namespace ProxyService.Controllers
+{
+    public class CartController : ApiController
+    {
+        private readonly HttpClient _httpClient;
+        private readonly PathHelper _pathHelper;
+        public CartController()
+        {
+            _httpClient = new HttpClient();
+            _pathHelper = new PathHelper();
+        }
+
+        [HttpGet]
+        public Task<HttpResponseMessage> Content()
+        {
+            return _httpClient.GetAsync($"{Service_Url.Cart}{_pathHelper.Paths.Get("cartContent")}");
+        }
+    }
+}
---
+++ BE/SessionService/DAL/CartContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using SessionService.Models;
+
+namespace SessionService.DAL
+{
+    public class CartContext : DbContext
+    {
+        public CartContext() : base("cartContext")
+        {
+        }
+
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+
+    }
+}
